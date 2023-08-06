671453b0aa6ca88e209e3ae37a0811e58ebb813f
Mark <wernsdorfer@gmail.com>
2023-08-06 10:55:31 +0200

    user interface

--- user_interface.py
+++ user_interface.py
@@ -1,4 +1,6 @@
 # coding=utf-8
+import datetime
+
 import nicegui
 
 
@@ -8,43 +10,102 @@ def page_layout():
         nicegui.ui.label("Details").style('font-size: 20px')
         nicegui.ui.button(text="memory", on_click=lambda: right_drawer.toggle(), icon='arrow_right').props('flat color=white')
 
-    with nicegui.ui.row().style("width: 100%"):  # main page
+    with nicegui.ui.row().style("width: 100%; position: absolute; left: 0; top: 0; bottom: 0; padding: 20px").classes("flex-col"):  # main page
         with nicegui.ui.column().style("width: 100%").classes('items-center justify-center'):
-            nicegui.ui.label("Develop a new anti cancer drug.")
-            nicegui.ui.label("[This is the progress report.]")
+            nicegui.ui.label("Develop a new anti cancer drug.").classes("text-2xl")
+            nicegui.ui.label(
+                "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, "
+                "sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor "
+                "sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, "
+                "sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor "
+                "sit amet. [This is the progress report.]"
+            )
 
-        with nicegui.ui.scroll_area().style('background-color: #f0f4fa; min-height: 70vh') as protocol:
-            for i in range(50):
-                with nicegui.ui.expansion(text=f"Step {i}", value=i == 49):
-                    nicegui.ui.label(f"Action: stuff")
-                    nicegui.ui.label(f"Result: stuff")
+        # grow dynamically
+        with nicegui.ui.scroll_area().style('background-color: #f0f4fa').classes("grow") as protocol:
+            for i in range(1, 51):
+                with nicegui.ui.row().classes("justify-between flex items-center"):
+                    nicegui.ui.label(f"natural thought {i}").classes("grow m-3 p-3 bg-blue-300 rounded-lg")
+                    nicegui.ui.button("details").classes("mx-5")
+                with nicegui.ui.row().classes("justify-between flex items-center"):
+                    nicegui.ui.label(f"natural result {i}").classes("grow m-3 p-3 bg-yellow-200 rounded-lg")
+                    nicegui.ui.button("details").classes("mx-5")
+                nicegui.ui.separator().classes("my-5")
             # protocol.scroll_to(percent=100)
 
-        with nicegui.ui.row().style('background-color: #f0f4fa'):
+        with nicegui.ui.row().style('background-color: #f0f4fa; width: 100%').classes('justify-around'):
             nicegui.ui.button("Pause")
             nicegui.ui.button("Cancel")
 
-    with nicegui.ui.left_drawer(top_corner=False, bottom_corner=False).style('background-color: #d7e3f4') as left_drawer:
-        nicegui.ui.button("new agent")
-        nicegui.ui.separator()
-        [nicegui.ui.button(f"Agent {i}: finished", color="positive") for i in range(3)]
-        [nicegui.ui.button(f"Agent {i}: working", color="info") for i in range(3)]
-        [nicegui.ui.button(f"Agent {i}: paused", color="warning") for i in range(3)]
+    with nicegui.ui.left_drawer(top_corner=False, bottom_corner=False).style('background-color: #d7e3f4').classes("flex flex-col items-center") as left_drawer:
+        nicegui.ui.button("New task")
+        nicegui.ui.separator().classes("my-5")
+        columns = [
+            {"name": "id", "label": "ID", "field": "id", "required": True, "align": "left", "type": "number"},
+            {"name": "task", "label": "Task", "field": "name", "required": True, "align": "left", "type": "text"},
+            {"name": "status", "label": "Status", "field": "status", "required": True, "align": "left", "type": "text"},
+        ]
+        rows = [
+            {"id": 1, "name": "task", "status": "finished", "color": "positive"},
+            {"id": 2, "name": "task", "status": "pending", "color": "warning"},
+            {"id": 3, "name": "task", "status": "working", "color": "info"},
+            {"id": 4, "name": "task", "status": "paused", "color": "accent"},
+            {"id": 5, "name": "task", "status": "cancelled", "color": "negative"},
+        ]
+        nicegui.ui.table(columns=columns, rows=rows, row_key="agent_no").classes("w-full")
 
-    with nicegui.ui.right_drawer(top_corner=False, bottom_corner=False).style('background-color: #ebf1fa') as right_drawer:
-        nicegui.ui.label("local")
-        [nicegui.ui.label(f"Fact {i}: show (approve?), remove") for i in range(3)]
-        [nicegui.ui.label(f"Action {i}: show (approve?), remove") for i in range(3)]
+    with nicegui.ui.right_drawer(top_corner=False, bottom_corner=False).style('background-color: #ebf1fa').props(":width=\"500\"") as right_drawer:
+        with nicegui.ui.tabs() as tabs:
+            local_memory = nicegui.ui.tab("local", "Local (ID 3)")
+            global_memory = nicegui.ui.tab("global", "Global")
 
-        nicegui.ui.separator()
+        with nicegui.ui.tab_panels(tabs, value=local_memory):
+            with nicegui.ui.tab_panel(local_memory):
+                with nicegui.ui.row().classes("justify-between"):
+                    with nicegui.ui.scroll_area().style("width: 45%"):
+                        columns = [
+                            {"name": "fact", "label": "Fact", "field": "fact", "required": True, "align": "left", "type": "text"}
+                        ]
+                        rows = [
+                            {"fact": f"fact {i}"} for i in range(1, 51)
+                        ]
+                        # details (remove?, persist?)
+                        nicegui.ui.table(columns=columns, rows=rows, row_key="fact")
 
-        nicegui.ui.label("global")
-        [nicegui.ui.label(f"Fact {i}: show, remove") for i in range(3)]
-        [nicegui.ui.label(f"Action {i}: show, remove") for i in range(3)]
+                    with nicegui.ui.scroll_area().style("width: 45%"):
+                        columns = [
+                            {"name": "action", "label": "Action", "field": "action", "required": True, "align": "left", "type": "text"}
+                        ]
+                        rows = [
+                            {"action": f"action {i}"} for i in range(1, 51)
+                        ]
+                        # details (remove?, persist?)
+                        nicegui.ui.table(columns=columns, rows=rows, row_key="action")
 
+            with nicegui.ui.tab_panel(global_memory):
+                with nicegui.ui.row().classes("justify-between"):
+                    with nicegui.ui.scroll_area().style("width: 45%"):
+                        columns = [
+                            {"name": "fact", "label": "Fact", "field": "fact", "required": True, "align": "left", "type": "text"}
+                        ]
+                        rows = [
+                            {"fact": f"fact {i}"} for i in range(1, 51)
+                        ]
+                        # details (remove?, persist?)
+                        nicegui.ui.table(columns=columns, rows=rows, row_key="fact")
+
+                    with nicegui.ui.scroll_area().style("width: 45%"):
+                        columns = [
+                            {"name": "action", "label": "Action", "field": "action", "required": True, "align": "left", "type": "text"}
+                        ]
+                        rows = [
+                            {"action": f"action {i}"} for i in range(1, 51)
+                        ]
+                        # details (remove?)
+                        nicegui.ui.table(columns=columns, rows=rows, row_key="action")
 
     with nicegui.ui.footer().style('background-color: #3874c8'):
-        nicegui.ui.label("Status update from agents, incl. agent number")
+        nicegui.ui.label("Status updates from agents, incl. agent number")
 
 
 def main() -> None:
