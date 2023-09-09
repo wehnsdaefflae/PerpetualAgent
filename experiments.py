862a3c6936de266421eaa239e28c6c55c7fc6e5a
Mark <wernsdorfer+github@gmail.com>
2023-09-09 10:08:05 +0200

    starting view reimplementation

--- experiments.py
+++ experiments.py
@@ -1,26 +1,117 @@
 # coding=utf-8
-from __future__ import annotations
+from dataclasses import dataclass
+
+import openai
+
+from utils.misc import segment_text
 
 
-class A:
-    def __init__(self) -> None:
-        print("init")
-
-    def __enter__(self) -> A:
-        print("enter")
-        return self
-
-    def __exit__(self, exc_type: any, exc_value: any, traceback: any) -> None:
-        print("exit")
+@dataclass(frozen=True)
+class Response:
+    output: str
+    summary: str
 
 
-def main() -> None:
-    with A() as a:
-        print("first with")
-
-    with a:
-        print("second with")
+def indent(text: str, indent_str: str = "    ") -> str:
+    return "\n".join(f"{indent_str}{line}" for line in text.splitlines())
 
 
-if __name__ == "__main__":
-    main()
+def summarize(text: str, *args: any, context: str | None = None, **kwargs: any) -> str:
+    while True:
+        if context is None:
+            context_prompt = ""
+            instruction = "Summarize the above text in the outermost `Content` tag.\n"
+
+        else:
+            context_prompt = (
+                f"<DontRepeat>\n"
+                f"{indent(context)}\n"
+                f"</DontRepeat>\n"
+                f"\n"
+            )
+            instruction = (
+                "Summarize the above text in the outermost `Content` tag. In your summary, consider the information in the outermost `DontRepeat` tag as already known to the reader."
+            )
+
+        summarize_prompt = (
+            f"{context_prompt}"
+            f"<Content>\n"
+            f"{indent(text)}\n"
+            f"</Content>\n"
+            f"\n"
+            f"{instruction}"
+        )
+
+        message = {"role": "user", "content": summarize_prompt}
+        try:
+            response_message = openai.ChatCompletion.create(*args, messages=[message], **kwargs)
+            first_message = response_message.choices[0]
+            output = first_message.content
+            return output
+
+        except openai.error.OpenAIError as e:
+            if not e.error("too long"):  # todo: thats sth else
+                raise e
+
+            sub_context = None
+            summaries = list()
+            for i, each_segment in enumerate(segment_text(text)):
+                each_summary = summarize(each_segment, *args, context=sub_context, **kwargs)
+                summaries.append(each_summary)
+                if i < 1:
+                    sub_context = each_summary
+                else:
+                    sub_context = summarize(f"{sub_context}\n{each_summary}", *args, **kwargs)
+
+            text = "\n".join(summaries)
+
+
+def prompt(instruction: str, payload: str, *args: any, summary: str | None = None, **kwargs: any) -> Response:
+    len_instruction = len(instruction)
+    if len_instruction >= 1_000:
+        raise ValueError("Instruction too long")
+
+    while True:
+        prompt_text = (
+            f"<Progress>\n"
+            f"{indent(summary)}\n"
+            f"</Progress>\n"
+            f"\n"
+            f"<Important>\n"
+            f"{indent(payload)}\n"
+            f"</Important>\n"
+            f"\n"
+            f"{instruction}"
+        )
+
+        message = {"role": "user", "content": prompt_text}
+        try:
+            response_message = openai.ChatCompletion.create(*args, messages=[message], **kwargs)
+            first_message = response_message.choices[0]
+            output = first_message.content
+
+            new_summary = summarize(
+                f"<ConversationLog>\n"
+                f"{indent(summary)}\n"
+                f"</ConversationLog>\n"
+                f"\n"
+                f"<UserRequest>\n"
+                f"{indent(instruction)}\n"
+                f"</UserRequest>\n"
+                f"\n"
+                f"<AssistantResponse>\n"
+                f"{indent(output)}\n"
+                f"</AssistantResponse>\n"
+                f"\n",
+                *args, **kwargs)
+
+            return Response(output, new_summary)
+
+        except openai.error.OpenAIError as e:
+            if not e.error("too long"):  # todo: sth else
+                raise e
+
+            if len(payload) < 1_000:
+                raise ValueError("Payload small, but request still too long.") from e
+
+            payload = summarize(payload, *args, context=summary, **kwargs)
